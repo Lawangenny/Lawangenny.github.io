4e9ad9d22c1150b003b577740f8f21c1eff2592a
Lawrienny Angenny Rocha Silva <angennyrs@gmail.com>
2022-09-12 08:24:01 -0300

    upload code

--- portifolio.js
+++ portifolio.js
@@ -13,4 +13,57 @@ window.addEventListener('scroll', function() {
 });
 
 
+//HOVER BUTTON CURSOS
+var buttonGarotas = document.getElementById("action-btn-G");
+var buttonCisco = document.getElementById("action-btn-C");
+var buttonJovens = document.getElementById("action-btn-J");
+
+var containerG = document.getElementById("container-G");
+var containerC = document.getElementById("container-C");
+var containerJ = document.getElementById("container-J");
+
+buttonGarotas.addEventListener("click", function(){
+
+    containerG.classList.toggle("mostra-caixa-garotas");
+    containerG.classList.toggle("esconde");
+
+});
+
+buttonCisco.addEventListener("click", function(){
+
+    containerC.classList.toggle("mostra-caixa-cisco");
+
+});
+
+buttonJovens.addEventListener("click", function(){
+
+    containerJ.classList.toggle("mostra-caixa-jovens");
+
+});
+
+
+//EMBRULHO BUTTON SKILLS
+var buttonA = document.getElementById("action-btn-a");
+var buttonB = document.getElementById("action-btn-b");
+
+var containerB = document.getElementById("container-b");
+var containerA = document.getElementById("container-a");
+
+
+buttonB.addEventListener("click", function(){
+
+    containerA.classList.remove("mostraA");
+    containerB.classList.toggle("mostraB");
+
+});
+
+
+buttonA.addEventListener("click", function(){
+
+    containerB.classList.remove("mostraB");
+    containerA.classList.toggle("mostraA");
+
+});
+
+
 
